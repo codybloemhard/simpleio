4aaf8ce9e71dd75397ffec4e2fe72259bf12eef2
Cody Bloemhard <cblmhrd@live.nl>
2021-09-03 21:31:26 +0200

    read file into buffer

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,13 @@ mod tests {
 
 use std::env;
 use std::path::{Path,PathBuf};
+use std::fs::File;
+use std::io::Read;
 
 /// Returns the home directory of the user as a String.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// extern crate simpleio as sio;
 /// println!("{:?}", sio::get_home_string());
@@ -43,7 +45,7 @@ pub fn get_home_string() -> Option<String>{
 /// Returns the home directory of the user as a std::path::PathBuf.
 ///
 /// # Example
-/// 
+///
 /// ```
 /// extern crate simpleio as sio;
 /// println!("{:?}", sio::get_home());
@@ -65,28 +67,24 @@ pub fn get_home() -> Option<PathBuf>{
 /// They should NOT be writen in the home directory as it clutters up the home dir and the .config was made for these files.
 /// However, many applications do this wrong.
 /// Just type ```la``` instead of ```ls``` inside your home dir to see evidence of that.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// extern crate simpleio as sio;
 /// println!("{:?}", sio::get_config());
 /// ```
 pub fn get_config() -> Option<PathBuf>{
-    let home = get_home();
-    if home.is_none(){
-        return Option::None;
-    }
-    let mut home = home.unwrap();
+    let mut home = get_home()?;
     home.push(".config");
-    return Option::Some(home);
+    Option::Some(home)
 }
 
 /// Returns true if the file exists, false if not.
 /// Directories are also files on unix.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// extern crate simpleio as sio;
 /// //asuming home is set and will return a value
@@ -104,9 +102,9 @@ pub fn file_exists(path: &Path) -> bool{
 pub enum DirStatus { Exists, Created, Error, }
 
 /// Creates the directory if it does not already exist.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// extern crate simpleio as sio;
 /// match sio::create_dir(sio::get_home().unwrap().as_path()){
@@ -126,3 +124,17 @@ pub fn create_dir(path: &Path) -> DirStatus{
     }
     return DirStatus::Error;
 }
+
+/// Reads file to the end into Vec<u8>
+pub fn read_file_into_buffer(file_path: &str) -> Result<Vec<u8>, std::io::Error>{
+    let mut buffer = Vec::new();
+    let mut file = match File::open(file_path) {
+        Ok(f) => f,
+        Err(e) => { return Err(e); }
+    };
+    match file.read_to_end(&mut buffer){
+        Ok(_) => Ok(buffer),
+        Err(e) => Err(e),
+    }
+}
+
