aaa7b55da2848ec0d19b2da02174397498dd93b8
Cody Bloemhard <codybloemhard@gmail.com>
2023-11-10 19:45:19 +0100

    updated: bit of a shine up;

--- src/bin.rs
+++ src/bin.rs
@@ -1,27 +1,26 @@
-extern crate simpleio as sio;
+use simpleio as sio;
 
 pub fn main(){
-    println!("haha yes");
+    test_get_home_string();
+    test_get_home();
+    test_file_exist();
     test_get_config();
 }
 
 fn test_get_home_string(){
-    extern crate simpleio as sio;
     println!("{:?}", sio::get_home_string());
 }
 
 fn test_get_home(){
-    extern crate simpleio as sio;
     println!("{:?}", sio::get_home());
 }
 
 fn test_file_exist(){
-    extern crate simpleio as sio;
     println!("{}", sio::file_exists(sio::get_home().unwrap().as_path()));
 }
 
+#[allow(dead_code)]
 fn test_create_dir(){
-    extern crate simpleio as sio;
     match sio::create_dir(sio::get_home().unwrap().as_path()){
         sio::DirStatus::Exists => println!("Home exists."),
         sio::DirStatus::Created => println!("Home created."),
@@ -30,6 +29,5 @@ fn test_create_dir(){
 }
 
 fn test_get_config(){
-    extern crate simpleio as sio;
     println!("{:?}", sio::get_config());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn file_exists_home(){
-        let t = super::file_exists(super::get_home().unwrap().as_path());
-        assert_eq!(t, true);
-    }
-    #[test]
-    fn file_exists_void(){
-        let p = std::path::PathBuf::from("/home/thisdoesnotexist");
-        let t = super::file_exists(&p.as_path());
-        assert_eq!(t, false);
-    }
-}
-
 use std::env;
-use std::path::{Path,PathBuf};
+use std::path::{ Path, PathBuf};
 use std::fs::File;
 use std::io::Read;
+use std::env::VarError;
 
 /// Returns the home directory of the user as a String.
 ///
 /// # Example
 ///
 /// ```
-/// extern crate simpleio as sio;
+/// use simpleio as sio;
 /// println!("{:?}", sio::get_home_string());
 /// ```
 /// It returns the same as:
-/// ```
+/// ```sh
 /// echo "$HOME"
 /// ```
-pub fn get_home_string() -> Option<String>{
-    match env::var("HOME"){
-        Ok(val) => Option::Some(val),
-        Err(_e) => Option::None,
-    }
+pub fn get_home_string() -> Result<String, VarError>{
+    env::var("HOME")
 }
 
 /// Returns the home directory of the user as a std::path::PathBuf.
@@ -47,17 +25,17 @@ pub fn get_home_string() -> Option<String>{
 /// # Example
 ///
 /// ```
-/// extern crate simpleio as sio;
+/// use simpleio as sio;
 /// println!("{:?}", sio::get_home());
 /// ```
 /// It returns the same as:
-/// ```
+/// ```sh
 /// echo "$HOME"
 /// ```
-pub fn get_home() -> Option<PathBuf>{
+pub fn get_home() -> Result<PathBuf, VarError>{
     match env::var("HOME"){
-        Ok(val) => Option::Some(PathBuf::from(val)),
-        Err(_e) => Option::None,
+        Ok(val) => Ok(PathBuf::from(val)),
+        Err(e) => Err(e)
     }
 }
 
@@ -71,13 +49,13 @@ pub fn get_home() -> Option<PathBuf>{
 /// # Example
 ///
 /// ```
-/// extern crate simpleio as sio;
+/// use simpleio as sio;
 /// println!("{:?}", sio::get_config());
 /// ```
-pub fn get_config() -> Option<PathBuf>{
+pub fn get_config() -> Result<PathBuf, VarError>{
     let mut home = get_home()?;
     home.push(".config");
-    Option::Some(home)
+    Ok(home)
 }
 
 /// Returns true if the file exists, false if not.
@@ -86,27 +64,23 @@ pub fn get_config() -> Option<PathBuf>{
 /// # Example
 ///
 /// ```
-/// extern crate simpleio as sio;
+/// use simpleio as sio;
 /// //asuming home is set and will return a value
 /// println!("{}", sio::file_exists(sio::get_home().unwrap().as_path()));
 /// ```
 pub fn file_exists(path: &Path) -> bool{
-    let metadata = std::fs::metadata(path);
-    match metadata{
-        Ok(_x) => true,
-        Err(_y) => false,
-    }
+    std::fs::metadata(path).is_ok()
 }
 
 /// Enum for possible outcomes when creating a directory
-pub enum DirStatus { Exists, Created, Error, }
+pub enum DirStatus { Exists, Created, Error }
 
 /// Creates the directory if it does not already exist.
 ///
 /// # Example
 ///
 /// ```
-/// extern crate simpleio as sio;
+/// use simpleio as sio;
 /// match sio::create_dir(sio::get_home().unwrap().as_path()){
 ///     sio::DirStatus::Exists => println!("Home exists."),
 ///     sio::DirStatus::Created => println!("Home created."),
@@ -114,27 +88,52 @@ pub enum DirStatus { Exists, Created, Error, }
 /// }
 /// ```
 pub fn create_dir(path: &Path) -> DirStatus{
-    let metadata = std::fs::metadata(path);
-    if metadata.is_ok(){
+    if file_exists(path){
         return DirStatus::Exists;
     }
     let res = std::fs::create_dir_all(path);
     if res.is_ok(){
-        return DirStatus::Created;
+        DirStatus::Created
+    } else {
+        DirStatus::Error
     }
-    return DirStatus::Error;
 }
 
 /// Reads file to the end into Vec<u8>
-pub fn read_file_into_buffer(file_path: &str) -> Result<Vec<u8>, std::io::Error>{
+pub fn read_file_into_buffer(file_path: &Path) -> Result<Vec<u8>, std::io::Error>{
     let mut buffer = Vec::new();
-    let mut file = match File::open(file_path) {
-        Ok(f) => f,
-        Err(e) => { return Err(e); }
-    };
-    match file.read_to_end(&mut buffer){
-        Ok(_) => Ok(buffer),
-        Err(e) => Err(e),
+    let mut file = File::open(file_path)?;
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
+}
+
+/// Reads file to the end into String
+pub fn read_file_into_string(file_path: &str) -> Result<String, std::io::Error>{
+    let mut string = String::new();
+    let mut file = File::open(file_path)?;
+    file.read_to_string(&mut string)?;
+    Ok(string)
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn file_exists_home(){
+        let t = super::file_exists(super::get_home().unwrap().as_path());
+        assert!(t);
+    }
+
+    #[test]
+    fn file_exists_void(){
+        let p = std::path::PathBuf::from("/home/thisdoesnotexist");
+        let t = super::file_exists(p.as_path());
+        assert!(!t);
     }
 }
 
